3d265ab984399bc6cc63cc22008baeff94942b9b
Priyadarshini Gokuldass <priyadass.94@gmail.com>
2018-10-25 00:14:41 -0700

    minor changes

--- cosmicrayrejection.py
+++ cosmicrayrejection.py
@@ -1,4 +1,4 @@
-# Goal of the pipeline is to remove the unwanted cosmic ray from the final science image.
+#  Goal of the pipeline is to remove the unwanted cosmic ray from the final science image.
 from astropy import units as u
 import numpy as np
 from astropy.nddata import CCDData
@@ -7,7 +7,7 @@ from astropy.utils.data import get_pkg_data_filename
 from astropy.io import fits
 
 # getting the input image file and data.
-image_sci = get_pkg_data_filename('D:/archiveunziped/cosmic_ray_rejection/finalscience1.fits')
+image_sci = get_pkg_data_filename('/media/sf_VB_shared_files/archiveunziped/cosmic_ray_rejection/finalscience1.fits')
 image_sci, header = fits.getdata(image_sci, header=True, ext=0, cobbler=True)
 # print(image_sci)
 
@@ -15,19 +15,16 @@ image_sci, header = fits.getdata(image_sci, header=True, ext=0, cobbler=True)
 # gain and read out noise depends on the detector. So enter the appropriate value.
 
 data = CCDData(image_sci, unit='adu')
-data_with_deviation = ccdproc.create_deviation(data, gain=1.5 * u.electron/u.adu, readnoise= 10* u.electron)
+data_with_deviation = ccdproc.create_deviation(data, gain=2.6 * u.electron/u.adu, readnoise= 15* u.electron)
 
-gain_corrected = ccdproc.gain_correct(data_with_deviation, 1.5*u.electron/u.adu)
+gain_corrected = ccdproc.gain_correct(data_with_deviation, 2.6*u.electron/u.adu)
 
-# cleaning the image and removing the cosmic ray by two methods.
-# method one : by finding median.
-cr_cleaned = ccdproc.cosmicray_median(gain_corrected, mbox=11, rbox=11, gbox=5)
+# cleaning the image and removing the cosmic ray using lacosmic
 
-# method 2 by using lacosmic
-# cr_cleaned = ccdproc.cosmicray_lacosmic(gain_corrected, sigclip=5)
+cr_cleaned = ccdproc.cosmicray_lacosmic(gain_corrected, sigclip=5)
 
 # saving the output fits image.
-fits.writeto('D:/archiveunziped/ipopfiles/finalop/cosrayfinalsciop.fits', np.array(cr_cleaned), checksum=True)
+fits.writeto('/media/sf_VB_shared_files/archiveunziped/ipopfiles/finalop/cosrayfinalsciopla_gaincorrected.fits', np.array(cr_cleaned), header, checksum=True)
 
 # hooray!! end of code!! :)
 
